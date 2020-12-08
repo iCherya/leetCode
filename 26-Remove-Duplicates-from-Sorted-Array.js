b63d7c59061166255b3f44ca167a6e75678dcc03
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-12-08 17:29:52 +0200

    Remove Duplicates from Sorted Array

---
+++ 26-Remove-Duplicates-from-Sorted-Array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = (nums) => {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
