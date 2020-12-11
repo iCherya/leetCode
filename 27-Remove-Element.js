cfe342f8574a63fd994b1edf70f82d6c0212b7b9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-12-11 17:59:16 +0200

    27. Remove Element

---
+++ 27-Remove-Element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = (nums, val) => {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+
+    return nums.length;
+};
