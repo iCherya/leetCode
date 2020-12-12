a831c435d31f355a0030058a0c269bf73fab72d4
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-12-12 12:09:37 +0200

    865. Smallest Subtree with all the Deepest Nodes (upd)

--- 865-Smallest-Subtree-with-all-the-Deepest-Nodes.js
+++ 865-Smallest-Subtree-with-all-the-Deepest-Nodes.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
 const subtreeWithAllDeepest = (root) => {
     let depth = 0;
     let result;
