3814d017104d1a28e3e72ef97468316860918dd9
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-12-12 11:50:43 +0200

    66. Plus One

---
+++ 66-Plus-One.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        digits[i]++;
+
+        if (digits[i] > 9) {
+            if (i < 1) {
+                digits.unshift(1);
+                digits[i + 1] = 0;
+            } else {
+                digits[i] = 0;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return digits;
+};
