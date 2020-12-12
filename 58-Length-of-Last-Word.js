ed75853f52f6630f99db0eb6b38f1bddd1869088
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-12-12 11:26:43 +0200

    58. Length of Last Word (upd)

--- 58-Length-of-Last-Word.js
+++ 58-Length-of-Last-Word.js
@@ -5,9 +5,9 @@
 const lengthOfLastWord = (s) => {
     const arr = s.trim().split(' ');
 
-    if (arr[arr.length - 1]) {
-        return arr[arr.length - 1].length;
-    } else {
-        return 0;
-    }
+    return arr[arr.length - 1].length;
 };
+
+console.log(lengthOfLastWord('Hello World'));
+console.log(lengthOfLastWord(' '));
+console.log(lengthOfLastWord('a '));
