f83a85ecd96f6bc080f3409341ed75250b917d7d
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-05 19:02:35 +0200

    82. Remove Duplicates from Sorted List II

---
+++ 82-Remove-Duplicates-from-Sorted-List-II.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = (head) => {
+    let newNode = new ListNode(-1, head);
+    let temp = newNode;
+
+    while (head && head.next) {
+        if (head.val === head.next.val) {
+            while (head && head.next && head.val == head.next.val) {
+                head = head.next;
+            }
+            head = head.next;
+            temp.next = head;
+        } else {
+            temp = temp.next;
+            head = head.next;
+        }
+    }
+
+    return newNode.next;
+};
