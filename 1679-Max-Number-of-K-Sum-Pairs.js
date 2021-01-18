0fbb33e27c3111a0e052b0b22570a0c3c933f0ba
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-18 10:57:20 +0200

    1679. Max Number of K-Sum Pairs

---
+++ 1679-Max-Number-of-K-Sum-Pairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maxOperations = (nums, k) => {
+    const map = new Map();
+    let result = 0;
+
+    for (let number of nums)
+        if (number < k)
+            if (map.get(k - number)) {
+                map.set(k - number, map.get(k - number) - 1);
+                result++;
+            } else {
+                map.set(number, (map.get(number) || 0) + 1);
+            }
+
+    return result;
+};
+
+console.log(maxOperations([1, 2, 3, 4], 5));
