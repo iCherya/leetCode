7bbab202ad4eda1b4eb5b1a1ff4bc9c48ae6501a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-23 11:30:14 +0200

    1329. Sort the Matrix Diagonally

---
+++ 1329-Sort-the-Matrix-Diagonally.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+const diagonalSort = (mat) => {
+    const m = mat.length;
+    const n = mat[0].length;
+    const data = {};
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            const num = mat[i][j];
+            !data[i - j] && (data[i - j] = []);
+            data[i - j].push(num);
+        }
+    }
+
+    for (const diagonal of Object.values(data)) {
+        diagonal.sort((a, b) => a - b);
+    }
+
+    const result = mat.map((el) => new Array(n).fill(null));
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            result[i][j] = data[i - j].pop();
+        }
+    }
+
+    return result;
+};
