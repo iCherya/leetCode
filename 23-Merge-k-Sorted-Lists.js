edaf57dff82bc31bd47a4023c7296eb59d305cdd
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-01-24 11:02:25 +0200

    23. Merge k Sorted Lists

---
+++ 23-Merge-k-Sorted-Lists.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+const mergeKLists = (lists) => {
+    const nums = [];
+
+    for (let node of list) {
+        while (node) {
+            nums.push(node.val);
+            node = node.next;
+        }
+    }
+
+    if (nums.length === 0) return null;
+
+    nums.sort((a, b) => a - b);
+
+    const newNode = new ListNode(nums[0]);
+
+    let current = newNode;
+
+    for (let i = 1; i < nums.length; i++) {
+        current.next = new ListNode(nums[i]);
+        current = current.next;
+    }
+
+    return newNode;
+};
