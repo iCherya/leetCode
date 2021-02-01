99e4edf9204439db777c933d106662aad8ff144e
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-01 11:53:47 +0200

    374. Guess Number Higher or Lower

---
+++ 374-Guess-Number-Higher-or-Lower.js
@@ -0,0 +1,29 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const guessNumber = (n) => {
+    let from = 0;
+    let to = n;
+
+    while (from <= to) {
+        let middle = Math.floor((from + to) / 2);
+        const target = guess(middle);
+
+        if (target === 0) {
+            return middle;
+        }
+        if (target === 1) from = middle + 1;
+
+        if (target === -1) to = middle - 1;
+    }
+};
