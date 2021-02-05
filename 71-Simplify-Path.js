57405e7182e34e2659db1c4090110ad6f34f35cc
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-05 11:20:28 +0200

    71. Simplify Path

---
+++ 71-Simplify-Path.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+const simplifyPath = (path) => {
+    const folders = path.split('/').filter((el) => /\.\.|[^\s\.]/.test(el));
+    const stack = [];
+
+    for (const current of folders) {
+        if (current === '..') {
+            stack.pop();
+        } else {
+            stack.push(current);
+        }
+    }
+
+    return '/' + stack.join('/');
+};
