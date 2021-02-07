db0d624cd72576cdb931525db5aedd06a6f04a68
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-07 10:53:36 +0200

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ 1750-Minimum-Length-of-String-After-Deleting-Similar-Ends.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minimumLength = (s) => {
+    const process = (str) => {
+        if (str.length < 2) return str.length;
+
+        let left = 0;
+        let right = str.length - 1;
+
+        if (str[left] != str[right]) return str.length;
+
+        let letter = str[left];
+        for (let i = 1; i < str.length; i++) {
+            if (str[i] === letter) {
+                left++;
+            } else {
+                break;
+            }
+        }
+        for (let i = str.length - 1; i >= 1; i--) {
+            if (str[i] === letter) {
+                right--;
+            } else {
+                break;
+            }
+        }
+
+        str = str.slice(left + 1, right + 1);
+
+        return process(str);
+    };
+
+    return process(s);
+};
