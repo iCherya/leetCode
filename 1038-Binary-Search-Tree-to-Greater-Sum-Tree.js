fa661ea2fba4446469acc5cd202bcf053737e21a
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-02-09 14:21:51 +0200

    1038. Binary Search Tree to Greater Sum Tree

---
+++ 1038-Binary-Search-Tree-to-Greater-Sum-Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const bstToGst = (root) => {
+    let sum = 0;
+
+    const traverse = (root) => {
+        if (root) {
+            traverse(root.right);
+            root.val += sum;
+            sum = root.val;
+            traverse(root.left);
+        }
+    };
+
+    traverse(root);
+
+    return root;
+};
