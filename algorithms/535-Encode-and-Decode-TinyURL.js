97d289abda786faf6388e60ce5c60ca001435c77
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-03-15 10:28:49 +0200

    535. Encode and Decode TinyURL

---
+++ algorithms/535-Encode-and-Decode-TinyURL.js
@@ -0,0 +1,27 @@
+const map = {};
+const origin = 'https://tinyurl.com/';
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+const encode = (longUrl) => {
+    const key = Math.random().toString(36).slice(-5);
+    map[key] = longUrl;
+
+    return origin + key;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+const decode = (shortUrl) => {
+    const key = shortUrl.split('/').pop();
+
+    return map[key];
+};
