68e96cac4453392719d2d32f09e8096c240e7995
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-04-19 21:53:09 +0300

    377. Combination Sum IV

---
+++ algorithms/377-Combination-Sum-IV.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const combinationSum4 = (nums, target) => {
+    const dp = new Array(target + 1).fill(0);
+    dp[0] = 1;
+
+    for (let i = 1; i <= target; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i >= nums[j]) {
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+    }
+
+    return dp[target];
+};
