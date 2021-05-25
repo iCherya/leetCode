fcaaf2506a63e1f893c85059e34f032902ab2b3b
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-25 06:43:32 +0300

    709. To Lower Case Refactored

--- algorithms/709-To-Lower-Case.js
+++ algorithms/709-To-Lower-Case.js
@@ -2,4 +2,18 @@
  * @param {string} str
  * @return {string}
  */
-const toLowerCase = (str) => str.toLowerCase();
+const toLowerCase = (str) => {
+    let result = '';
+
+    for (const char of str) {
+        const code = char.charCodeAt(0);
+
+        if (code >= 65 && code < 90) {
+            result += String.fromCharCode(code + 32);
+        } else {
+            result += char;
+        }
+    }
+
+    return result;
+};
