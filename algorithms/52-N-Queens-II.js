0f827187ff25ba5267b24507fd0672bd2feeb5a0
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-05-29 10:27:54 +0300

    52. N-Queens II

---
+++ algorithms/52-N-Queens-II.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const totalNQueens = (n) => {
+    const board = new Array(n).fill(null).map(() => new Array(n).fill('.'));
+    const result = [];
+
+    const isValid = (row, col) => {
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') return false;
+        }
+
+        for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] === 'Q') return false;
+        }
+
+        for (let i = row - 1, j = col + 1; i >= 0 && j <= n - 1; i--, j++) {
+            if (board[i][j] === 'Q') return false;
+        }
+
+        return true;
+    };
+
+    const helper = (row) => {
+        if (row === n) {
+            const option = board.map((row) => row.join(''));
+            result.push(option);
+            return;
+        }
+
+        for (let col = 0; col < n; col++) {
+            if (isValid(row, col)) {
+                board[row][col] = 'Q';
+                helper(row + 1);
+                board[row][col] = '.';
+            }
+        }
+    };
+
+    helper(0);
+    return result.length;
+};
