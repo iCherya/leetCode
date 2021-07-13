bcc43178f7aefc1bcc2f551fcf2ae53d853604bb
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-07-13 07:56:03 +0300

    1929. Concatenation of Array [Easy]

---
+++ algorithms/1929-Concatenation-of-Array.js
@@ -0,0 +1,8 @@
+// 1929. Concatenation of Array [Easy]
+// https://leetcode.com/problems/concatenation-of-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const getConcatenation = (nums) => nums.concat(nums);
