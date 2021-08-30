bb9f156e74264bf5551f1a930869468aea4c73ab
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-30 08:39:31 +0300

    35. Search Insert Position [Easy]

--- algorithms/35-Search-Insert-Position.js
+++ algorithms/35-Search-Insert-Position.js
@@ -1,23 +1,13 @@
+// 35. Search Insert Position [Easy]
+// https://leetcode.com/problems/search-insert-position/
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 const searchInsert = (nums, target) => {
-    let left = 0;
-    let right = nums.length - 1;
+    const position = nums.reverse().findIndex((el) => el < target);
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] > target) {
-            right = --mid;
-        } else {
-            left = ++mid;
-        }
-    }
-
-    return left;
+    return position < 0 ? 0 : nums.length - position;
 };
