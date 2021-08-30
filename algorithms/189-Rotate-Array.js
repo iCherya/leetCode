b72992a1eda8082d2797170918fdcb2b19f76441
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-08-30 22:26:52 +0300

    189. Rotate Array [Medium] - update

--- algorithms/189-Rotate-Array.js
+++ algorithms/189-Rotate-Array.js
@@ -1,11 +1,15 @@
+// 189. Rotate Array [Medium]
+// https://leetcode.com/problems/rotate-array/
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const rotate = (nums, k) => {
-    while (k > 0) {
-        nums.unshift(nums.pop());
-        k--;
-    }
+    if (k > nums.length) k = k % nums.length;
+    if (nums.length === 1 || k === 0) return;
+
+    nums.unshift(...nums.slice(-k));
+    nums.splice(nums.length - k, k);
 };
