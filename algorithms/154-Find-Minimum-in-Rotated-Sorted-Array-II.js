668f2e2ea837545bedf3c8ca31a4b11b5509b569
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-10-23 15:19:40 +0300

    154. Find Minimum in Rotated Sorted Array II

---
+++ algorithms/154-Find-Minimum-in-Rotated-Sorted-Array-II.js
@@ -0,0 +1,8 @@
+// 154. Find Minimum in Rotated Sorted Array II
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = (nums) => Math.min(...nums);
