a621f054c9fa5f71121124b6f51dc537b7d5b0ca
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2021-12-30 05:54:19 +0200

    2119. A Number After a Double Reversal [Easy]

---
+++ algorithms/2119-A-Number-After-a-Double-Reversal.js
@@ -0,0 +1,8 @@
+// 2119. A Number After a Double Reversal [Easy]
+// https://leetcode.com/problems/a-number-after-a-double-reversal/
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+const isSameAfterReversals = (num) => num === 0 || !!(num % 10);
