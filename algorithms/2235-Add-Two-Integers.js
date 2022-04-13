8fe71681a91a24b51382d253d17e9895b7ae1dca
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-04-13 08:23:18 +0300

    2235. Add Two Integers [Easy]

---
+++ algorithms/2235-Add-Two-Integers.js
@@ -0,0 +1,9 @@
+// 2235. Add Two Integers [Easy]
+// https://leetcode.com/problems/add-two-integers/
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+const sum = (num1, num2) => num1 + num2;
